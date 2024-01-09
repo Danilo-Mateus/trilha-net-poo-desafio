3f85d416a1962ad0f88a6f2bcd6055b53ee281ab
Danilo Mateus <129683116+Danilo-Mateus@users.noreply.github.com>
2024-01-09 13:49:51 -0300

    desafio concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,26 @@
-﻿using DesafioPOO.Models;
+﻿using System;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+namespace DesafioPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Teste com Nokia
+            Nokia nokia = new Nokia("123456789", "Nokia 8", "IMEI123", "64GB");
+            nokia.Ligar();
+            nokia.ReceberLigacao();
+            nokia.InstalarAplicativo("WhatsApp");
+
+            Console.WriteLine();
+
+            // Teste com iPhone
+            Iphone iphone = new Iphone("987654321", "iPhone 12", "IMEI456", "128GB");
+            iphone.Ligar();
+            iphone.ReceberLigacao();
+            iphone.InstalarAplicativo("Instagram");
+
+            Console.ReadLine();
+        }
+    }
+}
